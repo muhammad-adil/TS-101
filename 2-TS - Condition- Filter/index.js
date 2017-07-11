aad5d54a2fcb8157f7250e90881db13ebf8e8b23
Adil <Muhammad-Adil@users.noreply.github.com>
2017-07-12 02:41:49 +0500

    Conditions, Filter

---
+++ 2-TS - Condition- Filter/index.js
@@ -0,0 +1,59 @@
+/*
+var abc = 10;
+
+if (typeof abc === 'number' ) {
+    console.log(true);
+}else if(abc === 'boolean') {
+    console.log(abc);
+}else if(abc === 'string') {
+    console.log( 'truea' );
+} else {
+    console.log(false);
+}
+*/
+var students = [
+    {
+        firstName: 'john',
+        lastName: 'doe',
+        age: 28,
+        email: 'john.doe@gmail.com'
+    },
+    {
+        firstName: 'john',
+        lastName: 'doe',
+        age: 18,
+        email: 'john.doe@gmail.com'
+    },
+    {
+        firstName: 'john',
+        lastName: 'doe',
+        age: 20,
+        email: 'john.doe@gmail.com'
+    },
+    {
+        firstName: 'john',
+        lastName: 'doe',
+        age: 19,
+        email: 'john.doe@gmail.com'
+    },
+    {
+        firstName: 'john25',
+        lastName: 'doe',
+        age: 25,
+        email: 'john.doe@gmail.com'
+    }
+];
+var a = students.filter(function (el) {
+    return el.age > 20 || el.age < 17;
+    //console.log (a);      
+});
+console.log(a);
+var b = students.filter(function (el) {
+    return el.age > 20 && el.age < 17;
+    //console.log (a);      
+});
+console.log(b);
+/** let a = students.filter(function(el) {
+   return el.age < 20 && el.age < 21;
+ }
+**/ 
