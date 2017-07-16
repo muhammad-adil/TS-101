b69ea91edad343e0542e55eb68561ea00050e2e4
Adil <Muhammad-Adil@users.noreply.github.com>
2017-07-16 23:21:08 +0500

    update classes

--- 3-TS-Classes/classes.js
+++ 3-TS-Classes/classes.js
@@ -21,3 +21,48 @@ console.log(yourFullName.FullName);
 //const h = new Human('john')
 //console.log('h :' , h.getName());
 // console.log('h :' , h.eat()); 
+
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Yaka = (function () {
+    function Yaka() {
+        this.name = 'Yaka';
+        console.log('hello from Yaka constructor');
+    }
+    //    hello():void {
+    //    console.log('hello parent')
+    // }
+    Yaka.prototype.yakaEngine = function () {
+        console.log('hello From Yaka Engine');
+    };
+    return Yaka;
+}());
+var Zaqa = (function (_super) {
+    __extends(Zaqa, _super);
+    function Zaqa() {
+        var _this = _super.call(this) || this;
+        _this.name = 'Zaqa';
+        //console.log('aqua')
+        // super()
+        //this.hello()
+        _this.zaqaEngine();
+        return _this;
+    }
+    Zaqa.prototype.zaqaEngine = function () {
+        _super.prototype.yakaEngine.call(this);
+        console.log('hello from Zaqa engine');
+    };
+    return Zaqa;
+}(Yaka));
+var a = new Yaka();
+var ab = new Zaqa();
+var aba = new Zaqa.name();
