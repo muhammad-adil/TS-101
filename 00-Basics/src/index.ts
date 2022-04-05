285c88a5956a7418259b1e3e566877212b726f7f
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-04-05 15:33:02 +0200

    Typescript revision

---
+++ 00-Basics/src/index.ts
@@ -0,0 +1,19 @@
+let id: number = 5;
+let company: string = "ABC";
+let isPublished: boolean = true;
+
+let x: any = "Any String or number or boolean etc";
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, true, "ABC", 2, 3, 4, 5];
+
+// Tuple
+let person: [number, string, boolean] = [1, "Adil", true];
+
+// Tuple Array
+let employee: [number, string][];
+employee = [
+  [1, "A"],
+  [2, "B"],
+  [3, "C"],
+];
